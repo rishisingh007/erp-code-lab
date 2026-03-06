88368b7f1f98816782e07e533ec19f288e2d8f8b
Rishi Pal Singh <rishi54321@gmail.com>
2026-03-06 12:46:23 +0530

    chore(theme): add MUI ThemeProvider and CssBaseline

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider, CssBaseline, createTheme } from '@mui/material'
 import './index.css'
 import App from './App.jsx'
 
+const theme = createTheme({
+  // Customize your theme here as needed
+})
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </StrictMode>,
 )
